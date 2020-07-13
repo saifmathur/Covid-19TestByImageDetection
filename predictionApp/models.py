005a3f4ea34e3ac3702ebf2491a8afcd03a9a271
saifmathur <saifmathur41@gmail.com>
2020-07-14 02:33:11 +0530

    sqlite not being used, model file cleared

--- predictionApp/models.py
+++ predictionApp/models.py
@@ -1,14 +1,1 @@
 from django.db import models
-from datetime import datetime
-
-# Create your models here.
-'''
-class InsertFile(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    image = models.ImageField()
-    created_at = models.DateTimeField(default=datetime.now, blank=True)
-
-    def __str__(self):
-        return self.name
-'''
