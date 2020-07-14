11716a9323bcf242542ff15ac8a349d58ecb93bb
saifmathur <saifmathur41@gmail.com>
2020-07-14 17:06:08 +0530

    no change

--- predictionApp/views.py
+++ predictionApp/views.py
@@ -2,16 +2,27 @@ from django.shortcuts import render
 from pymongo import MongoClient
 from gridfs import GridFS
 
-from tensorflow import keras
-#model = keras.models.load_model('\covidPrediction\covid19_model.h5')
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import load_img
+from PIL import Image
 
 
-#from django.core.files.storage import FileSystemStorage
+model = load_model('covidPredictionModel\covid19_model.h5')
+
+
+from django.core.files.storage import FileSystemStorage
 
 CLIENT = MongoClient('localhost',27017)
 DATABASE = CLIENT['CovidProject']
 COLLECTION = DATABASE['userData']
 
+
+def ImageManagement(imageObj):
+    var = Image.open(imageObj,"r")
+    var = var.resize((224,224))
+    #var.show()
+    return var
 
 # Create your views here.
 def index(request):
@@ -22,9 +33,15 @@ def predict(request):
         name = request.POST['name']
         age = request.POST['age']
         image = request.FILES['img']
-        open(image)
-        #imageFile = FileSystemStorage()
-        #imageFile.save(image.name, image)
+        imageFile = FileSystemStorage()
+        imgObj = imageFile.save(image.name, image)
+        
+        result = ImageManagement(imgObj)
+        pred = model.predict(result)
+        print(pred)
+        
+
+        
         fs = GridFS(DATABASE)
         image_id = fs.put(image) #for finding and keeping track
         dataDict = {
@@ -34,8 +51,7 @@ def predict(request):
             "file_id": image_id
         }
         dataInsertion = COLLECTION.insert_one(dataDict)
-        #print(image)
-        #print(request)
+        #print(pred)
     return render(request,'covidIndex.html')
 
 
